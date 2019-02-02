2da973110e7eedaf6689fb08134a160591e91062
Bastiaan Wildenberg <wildenberg84@gmail.com>
2019-02-02 21:53:30 +0100

    Minor changes, ready to branch

--- run_function.py
+++ run_function.py
@@ -1,7 +1,10 @@
 import types
+import sys
 
 def start(func):
     # NOTE: the merger of dicts (**) requires Python 3.5 or higher
+    assert sys.version_info >= (3, 5), 'You need Python 3.5 or higher to run this!'
+    
     # NOTE: merging dicts copies their values, the new dict is not a backed dict!
     sandbox = {**globals()} # create a sandbox dict, add the global namespace
 
